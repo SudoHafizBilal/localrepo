b0420a5c796cff5f634eb96af663762ae81c5de3
SudoHafizBilal <hafizmbilal.1786@gmail.com>
2024-06-12 16:06:54 +0500

    add both

--- in.c
+++ in.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
     printf("This is my new c repo\n")
-    printf("This is my feature1 branch\n")
+    printf("This is my feature1 branch (button)\n")
     return 0;
 }
